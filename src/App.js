81c965febc75fb55c49d348eebb41c75352857ef
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 01:06:55 -0500

    added new Container components, paired with state and passed through props for step navigation methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import { useState } from 'react';
-import { styled } from 'styled-components'
 import './App.css';
 import Header from './components/Header';
 import IntroContainer from './components/IntroContainer';
+import QuizContainer from './components/QuizContainer';
+import ResultsContainer from './components/ResultsContainer';
 
 const App = () => {
   // State to conditionally render each part of the quiz process
+  const [loading, setLoading] = useState(false);
   const [stage1, setStage1] = useState(true);
   const [stage2, setStage2] = useState(false);
   const [stage3, setStage3] = useState(false);
+
+  // Will need to store all the quiz answer state here
+  // Do I want to just store the answers as useState values for each of these? 
+  // Maybe I store a data-value on each question and depending on if it's true or false or multiple
+  // choice then I can decide the value
+
+
+  // Methods to move through quiz
+  const moveToQuiz = () => {
+    setStage1(false);
+    setStage2(true);
+  }
+
+  const moveToResults = () => {
+    setStage2(false);
+    setStage3(true);
+  }
+
   return (
+
     <div className="App">
       <Header />
-      {stage1 && <IntroContainer />}
-      {/* Will need a CTA / Intro component */}
-      {/* Will need a quiz page component set */}
-      {/* Will need a quiz results component set */}
+      <div className="Body">
+        {/* Will have a component here for "Loading" which will be a flash of the message for 1 second each time the page refreshes */}
+        { stage1 && <IntroContainer moveToQuiz={moveToQuiz} /> }
+        {/* QuizContainer is going to be ugly  */}
+        { stage2 && <QuizContainer moveToResults={moveToResults} /> }
+        { stage3 && <ResultsContainer /> }
+      </div>
     </div>
   );
 }
