b64fdc996f31113adc1daea813d6d1cbd280387e
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 00:03:20 -0500

    Added Header and IntroContainer components and setup flow stage state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import { styled } from 'styled-components'
 import './App.css';
+import Header from './components/Header';
+import IntroContainer from './components/IntroContainer';
 
 const App = () => {
+  // State to conditionally render each part of the quiz process
+  const [stage1, setStage1] = useState(true);
+  const [stage2, setStage2] = useState(false);
+  const [stage3, setStage3] = useState(false);
   return (
     <div className="App">
-      {/* Will need a header */}
+      <Header />
+      {stage1 && <IntroContainer />}
       {/* Will need a CTA / Intro component */}
       {/* Will need a quiz page component set */}
       {/* Will need a quiz results component set */}
-      <h1>Green Homes Now</h1>
     </div>
   );
 }
