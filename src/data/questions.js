cfe16ad9b60026206ada411e3105644484b53021
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 00:02:39 -0500

    initalized data files for the questions and review text data

---
+++ src/data/questions.js
@@ -0,0 +1,20 @@
+// Need to figure out the best way to structure this for easy validation when quiz is done
+export default questions = {
+    test: "test1"
+}
+
+// Possible Structures
+// const questions = {
+//     1: {
+//         question: "Is this a test question?",
+//         type: "multi-choice",
+//         answers: [
+//             "answer1",
+//             "answer2",
+//             "answer3",
+//             "answer4"
+//         ]
+//     }
+// }
+
+// Specifying the type will allow me to use a single question container type
---
+++ src/data/reviewText.js
@@ -0,0 +1,29 @@
+// I will store the information for the questions follow up and
+// the information for the other cards in this file.
+// The naive expectation is that this will allow me to use a single card -> potentially not though
+
+// This one will be difficult as I will need to match / render based on the score and state of the quiz
+export const questionFeedback = {
+    1: {
+        originalQuestion: "",
+        followUpText: ""
+    },
+    2: {
+        originalQuestion: "",
+        followUpText: ""
+    }
+}
+
+// Have a card for sharing w/ friends and family
+export const cardSpreadTheWord = {
+    cardTitle: "",
+    text: "", 
+    links: []
+}
+
+// Have a card that links to other organizations
+export const cardGetInvolved = {
+    cardTitle: "",
+    text: "",
+    links: []
+}
