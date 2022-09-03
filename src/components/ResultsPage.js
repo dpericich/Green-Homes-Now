5fd4a2fdce14557e2096da8864d71a45624039c9
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 14:09:04 -0500

    Updated ResultsPage with new child Container components

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -1,12 +1,40 @@
 import React from "react";
 import styled from "styled-components";
-import { questionFeedback, cardSpreadTheWord, cardGetInvolved } from '../data/reviewText';
+import ResultsContainer from "./ResultsContainer";
+import ShareContainer from "./ShareContainer";
+import ActNowContainer from "./ActNowContainer";
 
-const ResultsPage = () => {
+const ResultsPage = ({ score }) => {
+    const Container = styled.div`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: orangered;
+        width: 60%;
+
+        @media screen and (max-width: 1200px) {
+            width: 70%;
+        }
+
+        @media screen and (max-width: 900px) {
+            width: 70%;
+        }
+
+        @media screen and (max-width: 600px) {
+            width: 90%;
+        }
+    `
+
+
+    // Need to make sure that each of these has the same size of initial load content
+    // This will keep the heights of the items consistent
     // I want to have different colors for each container
-    // Maybe I'll end up custom creating each container, calling it here and passing in the data from my name
     return(
-        <div>I am the Results Container</div>
+        <Container>
+            <ResultsContainer score={score} />
+            <ShareContainer />
+            <ActNowContainer />
+        </Container>
     )
 };
 
