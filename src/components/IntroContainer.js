4d8134db805d961b3373c451e4684dc504dc9e2c
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 00:02:08 -0500

    Created skeleton component for IntroContainer

---
+++ src/components/IntroContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const IntroContainer = () => {
+    return(
+        <div>
+            I am the intro container
+        </div>
+    )
+};
+
+export default IntroContainer;
