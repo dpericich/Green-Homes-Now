ba8f617229096bbf74251ba6b6c454a448736def
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 01:05:36 -0500

    created Quiz and Results container components

---
+++ src/components/ResultsContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ResultsContainer = () => {
+    return(
+        <div>I am the Results Container</div>
+    )
+};
+
+export default ResultsContainer;
