635a0c1b96ac1b76f0f97cfcf0c1bbd3d5aa61a8
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 14:32:46 -0500

    updated styling and copy for the ResultsContainer

--- src/components/ResultsContainer.js
+++ src/components/ResultsContainer.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { questionFeedback } from '../data/reviewText'
 
 const ResultsContainer = ({ score }) => {
-    // Will get the score as a prop
+    const [expandCard, setExpandCard] = useState(false)
+
     const Container = styled.div`
         display: flex;
         flex-direction: column;
@@ -11,7 +12,7 @@ const ResultsContainer = ({ score }) => {
         justify-content: flex-start;
         background-color: white;
         margin: 2rem;
-        padding: 3rem 1.25rem;
+        padding: 3rem 1.75rem;
         width: 70%;
         box-shadow: 6px 6px 14px rgba(38, 215, 128, .6);
         border-radius: 5px;
@@ -39,6 +40,32 @@ const ResultsContainer = ({ score }) => {
         font-size: 1.5rem;
     `
 
+    const Button = styled.div`
+        background-color: var(--vivid-green);
+        font-size: 1.7rem;
+        color: white;
+        width: 25rem;
+        text-align: center;
+        padding: 1.5rem 0;
+        border-radius: 5px;
+        box-shadow: 3px 3px rgba(0,0,0, 0.15);
+
+        &:active {
+            transform: translateY(10px);
+        }
+
+
+        &:hover {
+            color: black;
+            cursor: pointer;
+            transform: scale(0.98);
+        }
+    `
+
+    const MoreInfo = styled.div`
+        color: red;
+    `
+
     // 2 Categories -> 1-3 score and 4-5 score
     const goodWorkText = () => {
         return `You got a ${score}! Nice job, but you can always make a bigger impact. Check out more ways to fight climate change below!`
@@ -51,7 +78,12 @@ const ResultsContainer = ({ score }) => {
         <Container>
             <CardTitle>Your Results</CardTitle>
             <CardText>{score > 3 ? goodWorkText() : needsImprovementText()}</CardText>
-            <div>Reducing Your Footprint (+)</div>
+            <Button onClick={() => setExpandCard(!expandCard)}>Reduce Your Footprint (+)</Button>
+            {/* Will break this into a separate component */}
+            {/* This will allow me to move the data to that component */}
+            { expandCard && <MoreInfo>
+                I am more info
+            </MoreInfo> } 
         </Container>
     )
 };
