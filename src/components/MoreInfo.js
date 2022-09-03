729df9c79de71af69afabf0ed9d5657188cfb1b8
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 14:37:07 -0500

    Created new MoreInfo component for ResultsContainer and ActNowContainer

---
+++ src/components/MoreInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MoreInfo = () => {
+    return(
+        <div>
+            I am the MoreInfo Item
+        </div>
+    )
+};
+
+export default MoreInfo;
