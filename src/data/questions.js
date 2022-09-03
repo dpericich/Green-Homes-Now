446e10a9342445a266a955869375b0d57a98ba4c
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 12:17:36 -0500

    Updated structure of questions data sctructures

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,20 +1,51 @@
 // Need to figure out the best way to structure this for easy validation when quiz is done
-export default questions = {
-    test: "test1"
-}
-
-// Possible Structures
-// const questions = {
-//     1: {
-//         question: "Is this a test question?",
-//         type: "multi-choice",
-//         answers: [
-//             "answer1",
-//             "answer2",
-//             "answer3",
-//             "answer4"
-//         ]
-//     }
+// Will I want to have separate questions? -> What is the point of this? 
+// export default questions = {
+//     test: "test1"
 // }
+
+export const questions = [
+    {
+        question: { text: "This is placeholder text", number: 1 },
+        answers: [
+            { text: "This is a example answer", value: 1 },
+            { text: "This is a example answer", value: 2 },
+            { text: "This is a example answer", value: 3 },
+            { text: "This is a example answer", value: 4 }
+        ]
+    },
+    {
+        question: { text: "This is placeholder text", number: 1 },
+        answers: [
+            { text: "This is a example answer", value: 0 },
+            { text: "This is a example answer", value: 4 }
+        ]
+    },
+    {
+        question: { text: "This is placeholder text", number: 1 },
+        answers: [
+            { text: "This is a example answer", value: 1 },
+            { text: "This is a example answer", value: 2 },
+            { text: "This is a example answer", value: 3 },
+            { text: "This is a example answer", value: 4 }
+        ]
+    },
+    {
+        question: { text: "This is placeholder text", number: 1 },
+        answers: [
+            { text: "This is a example answer", value: 1 },
+            { text: "This is a example answer", value: 2 },
+            { text: "This is a example answer", value: 3 },
+            { text: "This is a example answer", value: 4 }
+        ]
+    },
+    {
+        question: { text: "This is placeholder text", number: 1 },
+        answers: [
+            { text: "This is a example answer", value: 0 },
+            { text: "This is a example answer", value: 4 }
+        ]
+    }
+]
 
 // Specifying the type will allow me to use a single question container type
