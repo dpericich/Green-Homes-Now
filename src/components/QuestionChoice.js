402b8dc60558f247564e4a7a555d0f9e952dfc31
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-03 23:05:03 -0500

    Refactored QuestionChoice component to use question prop item passed in and updated styling

--- src/components/QuestionChoice.js
+++ src/components/QuestionChoice.js
@@ -7,6 +7,8 @@ const QuestionChoice = ({ question }) => {
         flex-direction: column;
         align-items: center;
         justify-content: flex-start;
+        align-items: flex-start;
+        width:100%;
         padding: 1rem 0 4rem;
         margin: 2rem;
         background-color: white;
@@ -14,41 +16,42 @@ const QuestionChoice = ({ question }) => {
         border-radius: 5px;
     `
 
-    const Question = styled.h3`
-        font-size: 2.3rem;
+    const QuestionText = styled.h3`
+        text-align: left;
+        font-size: 2.5rem;
+        padding: 0 1rem;
     `
 
     const AnswerContainer = styled.div`
         display: flex,
         flex-direction: row;
+        padding: 0 1.5rem;
     `
 
-    const AnswerOption = styled.input`
+    const AnswerInput = styled.input`
         color: red;
+        margin-right: 1.5rem;
+    `
+
+    const AnswerText = styled.label`
+        font-size: 2rem;
+
+        @media screen and (max-width: 600px) {
+            font-size: 1.5rem;
+        }
     `
 
     // Need to verify that this is radio so I can't have multiple elements
 
     return(
         <Container>
-            <Question>Placeholder Question Here</Question>
-            {/* Iteratre over the elements here */}
-            <AnswerContainer>
-                <AnswerOption type="checkbox" value="Answer Option 1"></AnswerOption>
-                <label>This is a placeholder for an answer.</label>
-            </AnswerContainer>
-            <AnswerContainer>
-                <AnswerOption type="checkbox" value="Answer Option 1"></AnswerOption>
-                <label>This is a placeholder for an answer.</label>
-            </AnswerContainer>
-            <AnswerContainer>
-                <AnswerOption type="checkbox" value="Answer Option 1"></AnswerOption>
-                <label>This is a placeholder for an answer.</label>
-            </AnswerContainer>
-            <AnswerContainer>
-                <AnswerOption type="checkbox" value="Answer Option 2"></AnswerOption>
-                <label>This is a placeholder for an answer.</label>
-            </AnswerContainer>
+            <QuestionText>{question.question.text}</QuestionText>
+            {question.answers.map(answer => (
+                <AnswerContainer>
+                    <AnswerInput type="checkbox" value={answer.value}></AnswerInput>
+                    <AnswerText>{answer.text}</AnswerText>
+                </AnswerContainer>
+            ))}
         </Container>
     )
 };
