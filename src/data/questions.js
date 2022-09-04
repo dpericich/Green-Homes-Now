f103f719cf4c4b51ab44cabacea1aa29b02523bb
Daniel Pericich <danielpericich@Daniels-MacBook-Pro.local>
2022-09-04 12:19:11 -0500

    added in all questions and answers

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,12 +1,6 @@
-// Need to figure out the best way to structure this for easy validation when quiz is done
-// Will I want to have separate questions? -> What is the point of this? 
-// export default questions = {
-//     test: "test1"
-// }
-
 export const questions = [
     {
-        question: { text: "When did you last change your AC's air filter?", number: 1 },
+        question: { text: "When did you last change your air conditioner's air filter?", number: 0 },
         answers: [
             { text: "Within the last month", value: 4 },
             { text: "2-3 months ago", value: 3 },
@@ -15,37 +9,61 @@ export const questions = [
         ]
     },
     {
-        question: { text: "This is placeholder text", number: 1 },
+        question: { text: "Do you have separate bins for trash, recycling and compost?", number: 1 },
         answers: [
-            { text: "This is a example answer", value: 0 },
-            { text: "This is a example answer", value: 4 }
+            { text: "No, I only have a trash can.", value: 0 },
+            { text: "Yes, I have separate bins for all of those items.", value: 4 }
         ]
     },
     {
-        question: { text: "This is placeholder text", number: 1 },
+        question: { text: "Are all of the light bulbs in your home LEDs?", number: 2 },
         answers: [
-            { text: "This is a example answer", value: 1 },
-            { text: "This is a example answer", value: 2 },
-            { text: "This is a example answer", value: 3 },
-            { text: "This is a example answer", value: 4 }
+            { text: "No, I don't want to buy LEDs.", value: 0 },
+            { text: "I'm not sure what light bulbs I have.", value: 1 },
+            { text: "I have a mix of incandescent and LED bulbs.", value: 2 },
+            { text: "I have all LED bulbs in my home.", value: 4 }
         ]
     },
     {
-        question: { text: "This is placeholder text", number: 1 },
+        question: { text: "Do you have energy efficient windows?", number: 3 },
         answers: [
-            { text: "This is a example answer", value: 1 },
-            { text: "This is a example answer", value: 2 },
-            { text: "This is a example answer", value: 3 },
-            { text: "This is a example answer", value: 4 }
+            { text: "No, I don't have energy efficient windows.", value: 0 },
+            { text: "Yes, I've installed energy efficient windows in my home.", value: 4 }
         ]
     },
     {
-        question: { text: "This is placeholder text", number: 1 },
+        question: { text: "Do you always wash your clothes with hot water?", number: 4 },
         answers: [
-            { text: "This is a example answer", value: 0 },
-            { text: "This is a example answer", value: 4 }
+            { text: "No, I wash my clothes in cold water.", value: 4 },
+            { text: "Yes, I wash my clothes in hot water.", value: 0 }
         ]
-    }
+    },
+    {
+        question: { text: "Can you see light coming into your home around exterior door frames or windows?", number: 5 },
+        answers: [
+            { text: "Yes, there are cracks around my windows or exterior doors and I can feel drafts coming from them.", value: 0 },
+            { text: "No, all of my windows and doors are gap free and there are no drafts coming from them.", value: 4 }
+        ]
+    },
+    {
+        question: { text: "Do you use smart power strips for your electronics?", number: 6 },
+        answers: [
+            { text: "No, I plug all of my heavily used electronics and chargers directly into the wall.", value: 0 },
+            { text: "Yes, I use smart power strips for all of my electronics.", value: 4 }
+        ]
+    },
+    {
+        question: { text: "Do you regularly have an empty fridge?", number: 7 },
+        answers: [
+            { text: "Yes, my fridge is usually empty between grocery trips.", value: 0 },
+            { text: "No, I always keep my fridge full either with groceries or drinks.", value: 4 }
+        ]
+    },
+    {
+        question: { text: "Do you run your dish washer empty or during the day?", number: 8 },
+        answers: [
+            { text: "Yes, I run my dishwasher during the day and whenever I feel like washing my favorite plate.", value: 0 },
+            { text: "No, I only run my dishwasher when it's full and only at night time.", value: 4 }
+        ]
+    },
 ]
-
-// Specifying the type will allow me to use a single question container type
