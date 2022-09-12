32b4a931326cb95a885954b84ecbbefb65890652
Daniel Pericich <danielpericich@daniels-mbp.lan>
2022-09-12 12:38:13 -0500

    Updates

--- src/utils/helperFunctions.js
+++
@@ -1,7 +0,0 @@
-export const calculateQuizScore = (array) => {
-    const POSSIBLE_POINTS = 36;
-    const sum = array.reduce((cv, ac) => ac += cv, 0)
-    const score = Math.round((sum / POSSIBLE_POINTS) * 5)
-
-    return score;
-}
